5ab0defbb08e4716eb0a5911fa62e12391593f24
TalesNogueira <talescabralnog@gmail.com>
2025-11-04 21:03:03 -0300

    add: pins bkp folder

---
+++ Pins/MIPS-online.tcl
@@ -0,0 +1,100 @@
+# Copyright (C) 1991-2013 Altera Corporation
+# Your use of Altera Corporation's design tools, logic functions 
+# and other software and tools, and its AMPP partner logic 
+# functions, and any output files from any of the foregoing 
+# (including device programming or simulation files), and any 
+# associated documentation or information are expressly subject 
+# to the terms and conditions of the Altera Program License 
+# Subscription Agreement, Altera MegaCore Function License 
+# Agreement, or other applicable license agreement, including, 
+# without limitation, that your use is for the sole purpose of 
+# programming logic devices manufactured by Altera and sold by 
+# Altera or its authorized distributors.  Please refer to the 
+# applicable agreement for further details.
+
+# Quartus II 64-Bit Version 13.0.1 Build 232 06/12/2013 Service Pack 1 SJ Web Edition
+# File: C:\Users\tales\Downloads\Workspace\Unicycle-MIPS-Based-Computer-System\Pins\MIPS-online.tcl
+# Generated on: Wed Sep 17 00:27:44 2025
+
+package require ::quartus::project
+
+set_location_assignment PIN_Y2 -to clk
+set_location_assignment PIN_Y16 -to switch_0
+set_location_assignment PIN_AD21 -to switch_1
+set_location_assignment PIN_AE16 -to switch_2
+set_location_assignment PIN_AD15 -to switch_3
+set_location_assignment PIN_AE15 -to switch_4
+set_location_assignment PIN_AC19 -to switch_5
+set_location_assignment PIN_AF16 -to switch_6
+set_location_assignment PIN_AD19 -to switch_7
+set_location_assignment PIN_AF15 -to switch_8
+set_location_assignment PIN_AF24 -to switch_9
+set_location_assignment PIN_AE21 -to switch_10
+set_location_assignment PIN_AF25 -to switch_11
+set_location_assignment PIN_AC22 -to switch_12
+set_location_assignment PIN_AE22 -to switch_13
+set_location_assignment PIN_AF21 -to switch_14
+set_location_assignment PIN_AF22 -to switch_15
+set_location_assignment PIN_AG25 -to switch_hold
+set_location_assignment PIN_H15 -to led_hold
+set_location_assignment PIN_F17 -to led_clock
+set_location_assignment PIN_E21 -to led_input
+set_location_assignment PIN_AB22 -to push_input_confirm
+set_location_assignment PIN_AC15 -to push_manual_clock
+set_location_assignment PIN_G18 -to hex_0_0
+set_location_assignment PIN_F22 -to hex_0_1
+set_location_assignment PIN_E17 -to hex_0_2
+set_location_assignment PIN_L26 -to hex_0_3
+set_location_assignment PIN_L25 -to hex_0_4
+set_location_assignment PIN_J22 -to hex_0_5
+set_location_assignment PIN_H22 -to hex_0_6
+set_location_assignment PIN_M24 -to hex_1_0
+set_location_assignment PIN_Y22 -to hex_1_1
+set_location_assignment PIN_W21 -to hex_1_2
+set_location_assignment PIN_W22 -to hex_1_3
+set_location_assignment PIN_W25 -to hex_1_4
+set_location_assignment PIN_U23 -to hex_1_5
+set_location_assignment PIN_U24 -to hex_1_6
+set_location_assignment PIN_AA25 -to hex_2_0
+set_location_assignment PIN_AA26 -to hex_2_1
+set_location_assignment PIN_Y25 -to hex_2_2
+set_location_assignment PIN_W26 -to hex_2_3
+set_location_assignment PIN_Y26 -to hex_2_4
+set_location_assignment PIN_W27 -to hex_2_5
+set_location_assignment PIN_W28 -to hex_2_6
+set_location_assignment PIN_V21 -to hex_3_0
+set_location_assignment PIN_U21 -to hex_3_1
+set_location_assignment PIN_AB20 -to hex_3_2
+set_location_assignment PIN_AA21 -to hex_3_3
+set_location_assignment PIN_AD24 -to hex_3_4
+set_location_assignment PIN_AF23 -to hex_3_5
+set_location_assignment PIN_Y19 -to hex_3_6
+set_location_assignment PIN_AB19 -to hex_4_0
+set_location_assignment PIN_AA19 -to hex_4_1
+set_location_assignment PIN_AG21 -to hex_4_2
+set_location_assignment PIN_AH21 -to hex_4_3
+set_location_assignment PIN_AE19 -to hex_4_4
+set_location_assignment PIN_AF19 -to hex_4_5
+set_location_assignment PIN_AE18 -to hex_4_6
+set_location_assignment PIN_AD18 -to hex_5_0
+set_location_assignment PIN_AC18 -to hex_5_1
+set_location_assignment PIN_AB18 -to hex_5_2
+set_location_assignment PIN_AH19 -to hex_5_3
+set_location_assignment PIN_AG19 -to hex_5_4
+set_location_assignment PIN_AF18 -to hex_5_5
+set_location_assignment PIN_AH18 -to hex_5_6
+set_location_assignment PIN_AA17 -to hex_6_0
+set_location_assignment PIN_AB16 -to hex_6_1
+set_location_assignment PIN_AA16 -to hex_6_2
+set_location_assignment PIN_AB17 -to hex_6_3
+set_location_assignment PIN_AB15 -to hex_6_4
+set_location_assignment PIN_AA15 -to hex_6_5
+set_location_assignment PIN_AC17 -to hex_6_6
+set_location_assignment PIN_AD17 -to hex_7_0
+set_location_assignment PIN_AE17 -to hex_7_1
+set_location_assignment PIN_AG17 -to hex_7_2
+set_location_assignment PIN_AH17 -to hex_7_3
+set_location_assignment PIN_AF17 -to hex_7_4
+set_location_assignment PIN_AG18 -to hex_7_5
+set_location_assignment PIN_AA14 -to hex_7_6
+set_location_assignment PIN_G19 -to led_negative
